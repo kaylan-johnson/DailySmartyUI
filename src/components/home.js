7ea5dcc029094a3cf5cef856a6207e7363d13bbc
kaylan-johnson <drkaylan@gmail.com>
2019-07-10 16:26:27 -0600

    modified searchbar component to take in props

--- src/components/home.js
+++ src/components/home.js
@@ -5,12 +5,20 @@ import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
 
 export default class Home extends Component {
+
+
+  handleSearchbarSubmit(query) {
+    console.log('trying to handle submit for query', query);
+    this.props.history.push('/results');
+}
+
+
   render() {
     return (
       <div>
         <div>
           <Logo/>
-          <SearchBar/>
+          <SearchBar onSubmit={(query) => this.handleSearchbarSubmit(query)}/>
           <RecentPosts/>
         </div>
       </div>

00a9e424106868b03528c13852a169663acad8fd
kaylan-johnson <drkaylan@gmail.com>
2019-07-10 08:58:02 -0600

    setup redux devtools and fixed reducer

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import thunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 import "./style/main.scss";
 
 import Home from './components/home';

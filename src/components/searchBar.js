8bd9901fdc3179fa0b86f422a9c10502a3a5f312
kaylan-johnson <drkaylan@gmail.com>
2019-07-11 14:29:07 -0600

    styled the serachbar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -18,8 +18,12 @@ class SearchBar extends Component {
 
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <div className="search-bar__wrapper">
                 <Field name="query" component={this.renderInput}/>
+                <p>Press return to search</p>
+                </div>
             </form>
+
         )
     }
 }

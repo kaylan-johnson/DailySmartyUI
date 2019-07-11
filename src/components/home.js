ae36a1c75764e6d0ff99df880cafa07a7bbe4254
kaylan-johnson <drkaylan@gmail.com>
2019-07-11 10:00:20 -0600

    modified fetchpostswithquery

--- src/components/home.js
+++ src/components/home.js
@@ -12,8 +12,10 @@ class Home extends Component {
 
 
   handleSearchBarSubmit(query) {
-    this.props.fetchPostsWithQuery(query);
-    this.props.history.push('/results');
+    this.props.fetchPostsWithQuery(query, () => {
+      this.props.history.push('/results');
+
+    });
 }
 
 
